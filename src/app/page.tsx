cbe80a447ea284e5b962da47fd2f5fc49c4cd8cc
Shivansh270 <gupta.shivansh2709@gmail.com>
2024-05-11 12:59:34 +0530

    navbar completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,7 @@ import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
-    <main>
-      <h1 className="text-center 2xl mt-5">hello app</h1>
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <HeroSection />
     </main>
   );
